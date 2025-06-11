596f1ecbe6cc97f851f7e372405eb0ee7e26e207
deanosaurxoxo <odessakernpdecano@iskolarngbayan.pup.edu.ph>
2025-06-11 23:57:29 +0800

    My local UI changes before pulling master update

---
+++ PetVaccinationTrackerSystem-Project/Main Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetVaccinationTrackerSystem_Project
+{
+    public partial class mainMenu : Form
+    {
+        public mainMenu()
+        {
+            InitializeComponent();
+            loadform(new Home());
+        }
+        public void loadform(object Form)
+        {
+            if (this.mainPanel.Controls.Count > 0)
+                this.mainPanel.Controls.RemoveAt(0);
+            Form frm = Form as Form;
+            frm.TopLevel = false;
+            frm.Dock = DockStyle.Fill;
+            this.mainPanel.Controls.Add(frm);
+            this.mainPanel.Tag = frm;
+            frm.Show();
+        }
+
+        private void buttonHome_Click(object sender, EventArgs e)
+        {
+            loadform(new Home());
+            sidePanelHighlight.Height = buttonHome.Height;
+            sidePanelHighlight.Top = buttonHome.Top;
+        }
+
+        private void buttonPetProfile_Click(object sender, EventArgs e)
+        {
+            sidePanelHighlight.Height = buttonPetProfile.Height;
+            sidePanelHighlight.Top = buttonPetProfile.Top;
+        }
+
+        private void buttonVaccinationRecords_Click(object sender, EventArgs e)
+        {
+            sidePanelHighlight.Height = buttonVaccinationRecords.Height;
+            sidePanelHighlight.Top = buttonVaccinationRecords.Top;
+        }
+
+        private void buttonVaccinationReports_Click(object sender, EventArgs e)
+        {
+            sidePanelHighlight.Height = buttonVaccinationReports.Height;
+            sidePanelHighlight.Top = buttonVaccinationReports.Top;
+        }
+
+        private void buttonAlerts_Click(object sender, EventArgs e)
+        {
+            sidePanelHighlight.Height = buttonAlerts.Height;
+            sidePanelHighlight.Top = buttonAlerts.Top;
+        }
+
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            sidePanelHighlight.Height = buttonSettings.Height;
+            sidePanelHighlight.Top = buttonSettings.Top;
+        }
+    }
+}
