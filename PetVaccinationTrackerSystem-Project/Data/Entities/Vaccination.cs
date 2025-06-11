509a6db1662e0eaa4c8fcbc83042a999e91bb99e
johnangelomikhailareveche@iskolarngbayan.pup.edu.ph <mikhjnelo@gmail.com>
2025-06-11 21:39:36 +0800

    Created the Code-First Database Approach and Added the Models

---
+++ PetVaccinationTrackerSystem-Project/Data/Entities/Vaccination.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetVaccinationTrackerSystem_Project.Data.Entities
+{
+    public class Vaccination
+    {
+
+        [Key, StringLength(15)]
+        public required string VaccinationID { get; set; }
+
+        [StringLength(60)]
+        public string VaccineName { get; set; }
+
+        [StringLength(255)]
+        public string VaccineDesc { get; set; }
+
+        public float Frequency { get; set; }
+
+        // Navigation property list
+        public List<PetHealthRecords>? PetHealthRecords { get; set; }
+    }
+}
---
+++ PetVaccinationTrackerSystem-Project/Migrations/20250611132710_InitialCreate.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetVaccinationTrackerSystem_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClinicList",
+                columns: table => new
+                {
+                    ClinicID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ClinicName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    City = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    State = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    ZipCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClinicList", x => x.ClinicID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserList",
+                columns: table => new
+                {
+                    UserID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserEmail = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserPassword = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserRole = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserList", x => x.UserID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VaccinationList",
+                columns: table => new
+                {
+                    VaccinationID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    VaccineName = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    VaccineDesc = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Frequency = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VaccinationList", x => x.VaccinationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VeterinarianList",
+                columns: table => new
+                {
+                    VetID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    VetName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LicenseNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ClinicID = table.Column<string>(type: "nvarchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VeterinarianList", x => x.VetID);
+                    table.ForeignKey(
+                        name: "FK_VeterinarianList_ClinicList_ClinicID",
+                        column: x => x.ClinicID,
+                        principalTable: "ClinicList",
+                        principalColumn: "ClinicID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PetList",
+                columns: table => new
+                {
+                    PetID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    PetName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    OwnerFirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    OwnerLastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Species = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Breed = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Day = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    UserID = table.Column<string>(type: "nvarchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetList", x => x.PetID);
+                    table.ForeignKey(
+                        name: "FK_PetList_UserList_UserID",
+                        column: x => x.UserID,
+                        principalTable: "UserList",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserContactList",
+                columns: table => new
+                {
+                    ContactID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    ContactType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ContactValue = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserID = table.Column<string>(type: "nvarchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserContactList", x => x.ContactID);
+                    table.ForeignKey(
+                        name: "FK_UserContactList_UserList_UserID",
+                        column: x => x.UserID,
+                        principalTable: "UserList",
+                        principalColumn: "UserID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentList",
+                columns: table => new
+                {
+                    AppointmentID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Day = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    TimeOfAppointment = table.Column<TimeOnly>(type: "time", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    PetID = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    VetID = table.Column<string>(type: "nvarchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentList", x => x.AppointmentID);
+                    table.ForeignKey(
+                        name: "FK_AppointmentList_PetList_PetID",
+                        column: x => x.PetID,
+                        principalTable: "PetList",
+                        principalColumn: "PetID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppointmentList_VeterinarianList_VetID",
+                        column: x => x.VetID,
+                        principalTable: "VeterinarianList",
+                        principalColumn: "VetID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PetHealthRecordsList",
+                columns: table => new
+                {
+                    RecordID = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Month = table.Column<int>(type: "int", nullable: false),
+                    Day = table.Column<int>(type: "int", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    VaccinationID = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    VetID = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    PetID = table.Column<string>(type: "nvarchar(15)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetHealthRecordsList", x => x.RecordID);
+                    table.ForeignKey(
+                        name: "FK_PetHealthRecordsList_PetList_PetID",
+                        column: x => x.PetID,
+                        principalTable: "PetList",
+                        principalColumn: "PetID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PetHealthRecordsList_VaccinationList_VaccinationID",
+                        column: x => x.VaccinationID,
+                        principalTable: "VaccinationList",
+                        principalColumn: "VaccinationID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PetHealthRecordsList_VeterinarianList_VetID",
+                        column: x => x.VetID,
+                        principalTable: "VeterinarianList",
+                        principalColumn: "VetID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentList_PetID",
+                table: "AppointmentList",
+                column: "PetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentList_VetID",
+                table: "AppointmentList",
+                column: "VetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetHealthRecordsList_PetID",
+                table: "PetHealthRecordsList",
+                column: "PetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetHealthRecordsList_VaccinationID",
+                table: "PetHealthRecordsList",
+                column: "VaccinationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetHealthRecordsList_VetID",
+                table: "PetHealthRecordsList",
+                column: "VetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PetList_UserID",
+                table: "PetList",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserContactList_UserID",
+                table: "UserContactList",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VeterinarianList_ClinicID",
+                table: "VeterinarianList",
+                column: "ClinicID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppointmentList");
+
+            migrationBuilder.DropTable(
+                name: "PetHealthRecordsList");
+
+            migrationBuilder.DropTable(
+                name: "UserContactList");
+
+            migrationBuilder.DropTable(
+                name: "PetList");
+
+            migrationBuilder.DropTable(
+                name: "VaccinationList");
+
+            migrationBuilder.DropTable(
+                name: "VeterinarianList");
+
+            migrationBuilder.DropTable(
+                name: "UserList");
+
+            migrationBuilder.DropTable(
+                name: "ClinicList");
+        }
+    }
+}
