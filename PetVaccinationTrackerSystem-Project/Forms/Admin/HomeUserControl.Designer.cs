ba5cd8edc4aabb702b2c6b9f1135e13fd4b39ca2
johnangelomikhailareveche@iskolarngbayan.pup.edu.ph <mikhjnelo@gmail.com>
2025-06-24 18:33:54 +0800

    Updated Admin System: Moved Vet and Clinic List to their own Panels and cleared Home Panel

--- PetVaccinationTrackerSystem-Project/Forms/Admin/HomeUserControl.Designer.cs
+++ PetVaccinationTrackerSystem-Project/Forms/Admin/HomeUserControl.Designer.cs
@@ -28,152 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dgvVetList = new DataGridView();
-            dgvClinicList = new DataGridView();
-            label1 = new Label();
-            label2 = new Label();
-            txtboxSearchVet = new TextBox();
-            cmbboxFilterVet = new ComboBox();
-            btnSearchVet = new Button();
-            txtboxSearchClinic = new TextBox();
-            cmbboxFilterClinic = new ComboBox();
-            btnSearchClinic = new Button();
-            ((System.ComponentModel.ISupportInitialize)dgvVetList).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dgvClinicList).BeginInit();
             SuspendLayout();
-            // 
-            // dgvVetList
-            // 
-            dgvVetList.AllowUserToAddRows = false;
-            dgvVetList.BackgroundColor = Color.White;
-            dgvVetList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvVetList.Location = new Point(20, 57);
-            dgvVetList.MultiSelect = false;
-            dgvVetList.Name = "dgvVetList";
-            dgvVetList.ReadOnly = true;
-            dgvVetList.Size = new Size(733, 205);
-            dgvVetList.TabIndex = 0;
-            dgvVetList.DoubleClick += dgvVetList_DoubleClick;
-            // 
-            // dgvClinicList
-            // 
-            dgvClinicList.AllowUserToAddRows = false;
-            dgvClinicList.BackgroundColor = Color.White;
-            dgvClinicList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvClinicList.Location = new Point(20, 327);
-            dgvClinicList.MultiSelect = false;
-            dgvClinicList.Name = "dgvClinicList";
-            dgvClinicList.ReadOnly = true;
-            dgvClinicList.Size = new Size(733, 228);
-            dgvClinicList.TabIndex = 0;
-            dgvClinicList.DoubleClick += dgvClinicList_DoubleClick;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            label1.Location = new Point(20, 17);
-            label1.Name = "label1";
-            label1.Size = new Size(331, 37);
-            label1.TabIndex = 1;
-            label1.Text = "Registered Veterinarians";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            label2.Location = new Point(20, 287);
-            label2.Name = "label2";
-            label2.Size = new Size(244, 37);
-            label2.TabIndex = 1;
-            label2.Text = "Registered Clinics";
-            // 
-            // txtboxSearchVet
-            // 
-            txtboxSearchVet.Location = new Point(391, 28);
-            txtboxSearchVet.Name = "txtboxSearchVet";
-            txtboxSearchVet.Size = new Size(165, 23);
-            txtboxSearchVet.TabIndex = 2;
-            // 
-            // cmbboxFilterVet
-            // 
-            cmbboxFilterVet.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmbboxFilterVet.FormattingEnabled = true;
-            cmbboxFilterVet.Location = new Point(562, 28);
-            cmbboxFilterVet.Name = "cmbboxFilterVet";
-            cmbboxFilterVet.Size = new Size(121, 23);
-            cmbboxFilterVet.TabIndex = 3;
-            // 
-            // btnSearchVet
-            // 
-            btnSearchVet.Location = new Point(689, 27);
-            btnSearchVet.Name = "btnSearchVet";
-            btnSearchVet.Size = new Size(75, 23);
-            btnSearchVet.TabIndex = 4;
-            btnSearchVet.Text = "Search";
-            btnSearchVet.UseVisualStyleBackColor = true;
-            btnSearchVet.Click += btnSearchVet_Click;
-            // 
-            // txtboxSearchClinic
-            // 
-            txtboxSearchClinic.Location = new Point(391, 298);
-            txtboxSearchClinic.Name = "txtboxSearchClinic";
-            txtboxSearchClinic.Size = new Size(165, 23);
-            txtboxSearchClinic.TabIndex = 2;
-            // 
-            // cmbboxFilterClinic
-            // 
-            cmbboxFilterClinic.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmbboxFilterClinic.FormattingEnabled = true;
-            cmbboxFilterClinic.Location = new Point(562, 298);
-            cmbboxFilterClinic.Name = "cmbboxFilterClinic";
-            cmbboxFilterClinic.Size = new Size(121, 23);
-            cmbboxFilterClinic.TabIndex = 3;
-            // 
-            // btnSearchClinic
-            // 
-            btnSearchClinic.Location = new Point(689, 297);
-            btnSearchClinic.Name = "btnSearchClinic";
-            btnSearchClinic.Size = new Size(75, 23);
-            btnSearchClinic.TabIndex = 4;
-            btnSearchClinic.Text = "Search";
-            btnSearchClinic.UseVisualStyleBackColor = true;
-            btnSearchClinic.Click += btnSearchClinic_Click;
             // 
             // HomeUserControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            Controls.Add(dgvVetList);
-            Controls.Add(btnSearchClinic);
-            Controls.Add(btnSearchVet);
-            Controls.Add(cmbboxFilterClinic);
-            Controls.Add(txtboxSearchClinic);
-            Controls.Add(cmbboxFilterVet);
-            Controls.Add(txtboxSearchVet);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(dgvClinicList);
             Name = "HomeUserControl";
             Size = new Size(800, 590);
             Load += HomeUserControl_Load;
-            ((System.ComponentModel.ISupportInitialize)dgvVetList).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dgvClinicList).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private DataGridView dgvVetList;
-        private DataGridView dgvClinicList;
-        private Label label1;
-        private Label label2;
-        private TextBox txtboxSearchVet;
-        private ComboBox cmbboxFilterVet;
-        private Button btnSearchVet;
-        private TextBox txtboxSearchClinic;
-        private ComboBox cmbboxFilterClinic;
-        private Button btnSearchClinic;
     }
 }
---
+++ PetVaccinationTrackerSystem-Project/Forms/Admin/RegisteredClinicPanel.cs
@@ -0,0 +1,128 @@
+﻿using PetVaccinationTrackerSystem_Project.Data;
+using PetVaccinationTrackerSystem_Project.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetVaccinationTrackerSystem_Project.Forms.Admin
+{
+    public partial class RegisteredClinicPanel : UserControl
+    {
+        public void LoadClinicData()
+        {
+
+            if(!string.IsNullOrEmpty(txtboxSearchClinic.Text))
+            {
+                txtboxSearchClinic.Text = "";
+            }
+
+            using (var context = new ModelContext())
+            {
+
+                dgvClinicList.DataSource = context.ClinicList.ToList();
+
+                // If we want to hide a column then:
+                // dgvClinicList.Columns["ClinicID"].Visible = false;
+
+
+                // Adjust DataGridView Properties
+                dgvClinicList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dgvClinicList.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+                dgvClinicList.Columns["ClinicID"].HeaderText = "Clinic ID";
+                dgvClinicList.Columns["ClinicName"].HeaderText = "Clinic Name";
+                dgvClinicList.Columns["ZipCode"].HeaderText = "Zip Code";
+
+            }
+        }
+
+        public void LoadComboBoxWithData()
+        {
+            // For Filter Clinic Combo Box
+            cmbboxFilterClinic.Items.Add("All");
+            cmbboxFilterClinic.Items.Add("Street");
+            cmbboxFilterClinic.Items.Add("City");
+            cmbboxFilterClinic.Items.Add("State");
+            cmbboxFilterClinic.SelectedIndex = 0;
+        }
+
+        public RegisteredClinicPanel()
+        {
+            InitializeComponent();
+
+            LoadClinicData();
+            LoadComboBoxWithData();
+            dgvClinicList.ScrollBars = ScrollBars.Both;
+        }
+
+        private void dgvClinicList_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvClinicList.CurrentRow?.DataBoundItem is Clinic selectedClinic)
+            {
+
+                // Pass the reference to the Form
+                ClinicInfo clinicInfoForm = new ClinicInfo(selectedClinic);
+
+                // Refresh the data after closing the form
+                clinicInfoForm.FormClosed += (s, args) =>
+                {
+                    LoadClinicData();
+                };
+
+                clinicInfoForm.ShowDialog();
+            }
+        }
+
+        private void btnSearchClinic_Click(object sender, EventArgs e)
+        {
+            using (var context = new ModelContext())
+            {
+                // Read and Store in the values
+                string selectedFilterClinicOpt = cmbboxFilterClinic.SelectedItem?.ToString();
+                string txtboxSearchClinicText = txtboxSearchClinic.Text.Trim();
+
+                // Query
+                var query = context.ClinicList.AsQueryable();
+
+
+                if (!string.IsNullOrWhiteSpace(txtboxSearchClinicText))
+                {
+                    switch (selectedFilterClinicOpt)
+                    {
+                        case "Street":
+                            query = query.Where(v => v.Street.Contains(txtboxSearchClinicText));
+                            break;
+                        case "City":
+                            query = query.Where(v => v.City.Contains(txtboxSearchClinicText));
+                            break;
+                        case "State":
+                            query = query.Where(v => v.State.Contains(txtboxSearchClinicText));
+                            break;
+                        case "All":
+                            query = query.Where(
+                                v => v.Street.Contains(txtboxSearchClinicText) ||
+                                     v.City.Contains(txtboxSearchClinicText) ||
+                                     v.State.Contains(txtboxSearchClinicText)
+                                     );
+                            break;
+
+                        default:
+                            MessageBox.Show("Please select a valid filter option.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                    }
+
+                }
+
+                // Select the data to display in the DataGridView
+                var results = query.ToList();
+                dgvClinicList.DataSource = results;
+            }
+        }
+    }
+}
