e6e8987bcaf2f57dfadc2d7b8fc49445f194019a
deanosaurxoxo <odessakernpdecano@iskolarngbayan.pup.edu.ph>
2025-06-24 23:44:19 +0800

    Refined RegisterClinicPanel and RegisterVetPanel UI

--- PetVaccinationTrackerSystem-Project/Forms/Admin/RegisteredClinicPanel.Designer.cs
+++ PetVaccinationTrackerSystem-Project/Forms/Admin/RegisteredClinicPanel.Designer.cs
@@ -28,68 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnSearchClinic = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisteredClinicPanel));
             cmbboxFilterClinic = new ComboBox();
             txtboxSearchClinic = new TextBox();
             label2 = new Label();
             dgvClinicList = new DataGridView();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            btnSearchClinic = new ReaLTaiizor.Controls.HopeRoundButton();
             ((System.ComponentModel.ISupportInitialize)dgvClinicList).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
-            // 
-            // btnSearchClinic
-            // 
-            btnSearchClinic.Location = new Point(693, 24);
-            btnSearchClinic.Name = "btnSearchClinic";
-            btnSearchClinic.Size = new Size(75, 23);
-            btnSearchClinic.TabIndex = 9;
-            btnSearchClinic.Text = "Search";
-            btnSearchClinic.UseVisualStyleBackColor = true;
-            btnSearchClinic.Click += btnSearchClinic_Click;
             // 
             // cmbboxFilterClinic
             // 
             cmbboxFilterClinic.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbboxFilterClinic.Font = new Font("Segoe UI", 10F);
             cmbboxFilterClinic.FormattingEnabled = true;
-            cmbboxFilterClinic.Location = new Point(566, 25);
+            cmbboxFilterClinic.Location = new Point(537, 157);
             cmbboxFilterClinic.Name = "cmbboxFilterClinic";
-            cmbboxFilterClinic.Size = new Size(121, 23);
+            cmbboxFilterClinic.Size = new Size(140, 25);
             cmbboxFilterClinic.TabIndex = 8;
             // 
             // txtboxSearchClinic
             // 
-            txtboxSearchClinic.Location = new Point(395, 25);
+            txtboxSearchClinic.Font = new Font("Segoe UI", 10F);
+            txtboxSearchClinic.Location = new Point(156, 157);
             txtboxSearchClinic.Name = "txtboxSearchClinic";
-            txtboxSearchClinic.Size = new Size(165, 23);
+            txtboxSearchClinic.Size = new Size(367, 25);
             txtboxSearchClinic.TabIndex = 7;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            label2.Location = new Point(24, 14);
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(89, 85, 84);
+            label2.Location = new Point(24, 158);
             label2.Name = "label2";
-            label2.Size = new Size(244, 37);
+            label2.Size = new Size(125, 19);
             label2.TabIndex = 6;
-            label2.Text = "Registered Clinics";
+            label2.Text = "Filter and Search:";
             // 
             // dgvClinicList
             // 
             dgvClinicList.AllowUserToAddRows = false;
             dgvClinicList.BackgroundColor = Color.White;
             dgvClinicList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvClinicList.Location = new Point(24, 54);
+            dgvClinicList.Location = new Point(24, 195);
             dgvClinicList.MultiSelect = false;
             dgvClinicList.Name = "dgvClinicList";
             dgvClinicList.ReadOnly = true;
-            dgvClinicList.Size = new Size(733, 521);
+            dgvClinicList.Size = new Size(754, 380);
             dgvClinicList.TabIndex = 5;
             dgvClinicList.DoubleClick += dgvClinicList_DoubleClick;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 150);
+            panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 150);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnSearchClinic
+            // 
+            btnSearchClinic.BorderColor = Color.FromArgb(220, 223, 230);
+            btnSearchClinic.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnSearchClinic.DangerColor = Color.FromArgb(245, 108, 108);
+            btnSearchClinic.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnSearchClinic.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearchClinic.HoverTextColor = Color.White;
+            btnSearchClinic.InfoColor = Color.FromArgb(144, 147, 153);
+            btnSearchClinic.Location = new Point(688, 153);
+            btnSearchClinic.Name = "btnSearchClinic";
+            btnSearchClinic.PrimaryColor = Color.FromArgb(89, 85, 84);
+            btnSearchClinic.Size = new Size(90, 30);
+            btnSearchClinic.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnSearchClinic.TabIndex = 11;
+            btnSearchClinic.Text = "Search";
+            btnSearchClinic.TextColor = Color.White;
+            btnSearchClinic.WarningColor = Color.FromArgb(230, 162, 60);
+            btnSearchClinic.Click += btnSearchClinic_Click;
             // 
             // RegisteredClinicPanel
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             Controls.Add(btnSearchClinic);
+            Controls.Add(panel1);
             Controls.Add(cmbboxFilterClinic);
             Controls.Add(txtboxSearchClinic);
             Controls.Add(label2);
@@ -97,16 +137,19 @@
             Name = "RegisteredClinicPanel";
             Size = new Size(800, 590);
             ((System.ComponentModel.ISupportInitialize)dgvClinicList).EndInit();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button btnSearchClinic;
         private ComboBox cmbboxFilterClinic;
         private TextBox txtboxSearchClinic;
         private Label label2;
         private DataGridView dgvClinicList;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private ReaLTaiizor.Controls.HopeRoundButton btnSearchClinic;
     }
 }
--- PetVaccinationTrackerSystem-Project/Forms/Admin/RegisteredVetPanel.Designer.cs
+++ PetVaccinationTrackerSystem-Project/Forms/Admin/RegisteredVetPanel.Designer.cs
@@ -28,12 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisteredVetPanel));
             dgvVetList = new DataGridView();
-            btnSearchVet = new Button();
             cmbboxFilterVet = new ComboBox();
             txtboxSearchVet = new TextBox();
             label1 = new Label();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            btnSearchVet = new ReaLTaiizor.Controls.HopeRoundButton();
             ((System.ComponentModel.ISupportInitialize)dgvVetList).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // dgvVetList
@@ -41,62 +46,99 @@
             dgvVetList.AllowUserToAddRows = false;
             dgvVetList.BackgroundColor = Color.White;
             dgvVetList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvVetList.Location = new Point(27, 60);
+            dgvVetList.Location = new Point(24, 195);
             dgvVetList.MultiSelect = false;
             dgvVetList.Name = "dgvVetList";
             dgvVetList.ReadOnly = true;
-            dgvVetList.Size = new Size(733, 506);
+            dgvVetList.Size = new Size(754, 380);
             dgvVetList.TabIndex = 5;
             dgvVetList.DoubleClick += dgvVetList_DoubleClick;
-            // 
-            // btnSearchVet
-            // 
-            btnSearchVet.Location = new Point(696, 30);
-            btnSearchVet.Name = "btnSearchVet";
-            btnSearchVet.Size = new Size(75, 23);
-            btnSearchVet.TabIndex = 9;
-            btnSearchVet.Text = "Search";
-            btnSearchVet.UseVisualStyleBackColor = true;
-            btnSearchVet.Click += btnSearchVet_Click;
             // 
             // cmbboxFilterVet
             // 
             cmbboxFilterVet.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbboxFilterVet.Font = new Font("Segoe UI", 10F);
             cmbboxFilterVet.FormattingEnabled = true;
-            cmbboxFilterVet.Location = new Point(569, 31);
+            cmbboxFilterVet.Location = new Point(537, 157);
             cmbboxFilterVet.Name = "cmbboxFilterVet";
-            cmbboxFilterVet.Size = new Size(121, 23);
+            cmbboxFilterVet.Size = new Size(140, 25);
             cmbboxFilterVet.TabIndex = 8;
             // 
             // txtboxSearchVet
             // 
-            txtboxSearchVet.Location = new Point(398, 31);
+            txtboxSearchVet.Font = new Font("Segoe UI", 10F);
+            txtboxSearchVet.Location = new Point(156, 157);
             txtboxSearchVet.Name = "txtboxSearchVet";
-            txtboxSearchVet.Size = new Size(165, 23);
+            txtboxSearchVet.Size = new Size(367, 25);
             txtboxSearchVet.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            label1.Location = new Point(27, 20);
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(89, 85, 84);
+            label1.Location = new Point(24, 158);
             label1.Name = "label1";
-            label1.Size = new Size(331, 37);
+            label1.Size = new Size(125, 19);
             label1.TabIndex = 6;
-            label1.Text = "Registered Veterinarians";
+            label1.Text = "Filter and Search:";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 150);
+            panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 150);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnSearchVet
+            // 
+            btnSearchVet.BorderColor = Color.FromArgb(220, 223, 230);
+            btnSearchVet.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnSearchVet.DangerColor = Color.FromArgb(245, 108, 108);
+            btnSearchVet.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnSearchVet.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearchVet.HoverTextColor = Color.White;
+            btnSearchVet.InfoColor = Color.FromArgb(144, 147, 153);
+            btnSearchVet.Location = new Point(688, 153);
+            btnSearchVet.Name = "btnSearchVet";
+            btnSearchVet.PrimaryColor = Color.FromArgb(89, 85, 84);
+            btnSearchVet.Size = new Size(90, 30);
+            btnSearchVet.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnSearchVet.TabIndex = 11;
+            btnSearchVet.Text = "Search";
+            btnSearchVet.TextColor = Color.White;
+            btnSearchVet.WarningColor = Color.FromArgb(230, 162, 60);
+            btnSearchVet.Click += btnSearchVet_Click;
             // 
             // RegisteredVetPanel
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(dgvVetList);
+            BackColor = Color.White;
             Controls.Add(btnSearchVet);
+            Controls.Add(panel1);
+            Controls.Add(dgvVetList);
             Controls.Add(cmbboxFilterVet);
             Controls.Add(txtboxSearchVet);
             Controls.Add(label1);
             Name = "RegisteredVetPanel";
             Size = new Size(800, 590);
             ((System.ComponentModel.ISupportInitialize)dgvVetList).EndInit();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -104,9 +146,11 @@
         #endregion
 
         private DataGridView dgvVetList;
-        private Button btnSearchVet;
         private ComboBox cmbboxFilterVet;
         private TextBox txtboxSearchVet;
         private Label label1;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private ReaLTaiizor.Controls.HopeRoundButton btnSearchVet;
     }
 }
