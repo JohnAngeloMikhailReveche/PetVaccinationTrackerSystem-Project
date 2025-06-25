33547d23a9ffe2b42ce4fa7de2afd98540c9cff4
johnangelomikhailareveche@iskolarngbayan.pup.edu.ph <mikhjnelo@gmail.com>
2025-06-25 19:06:29 +0800

    Fixed Unhandled Exception in PasswordRequestForm and Added Partial Systems

---
+++ PetVaccinationTrackerSystem-Project/Forms/ReadWriteEmailSystemForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PetVaccinationTrackerSystem_Project.Forms
+{
+    partial class ReadWriteEmailSystemForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSend = new Button();
+            richtxtContent = new RichTextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            txtToUser = new TextBox();
+            txtTitle = new TextBox();
+            datetimeDateSent = new DateTimePicker();
+            SuspendLayout();
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new Point(604, 397);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(160, 41);
+            btnSend.TabIndex = 12;
+            btnSend.Text = "Send";
+            btnSend.UseVisualStyleBackColor = true;
+            btnSend.Click += btnSend_Click;
+            // 
+            // richtxtContent
+            // 
+            richtxtContent.Location = new Point(36, 122);
+            richtxtContent.Name = "richtxtContent";
+            richtxtContent.Size = new Size(728, 257);
+            richtxtContent.TabIndex = 11;
+            richtxtContent.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(274, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(142, 15);
+            label3.TabIndex = 8;
+            label3.Text = "To ( Provide the User ID ) :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Date Sent:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 15);
+            label1.TabIndex = 10;
+            label1.Text = "Subject:";
+            // 
+            // txtToUser
+            // 
+            txtToUser.Location = new Point(274, 83);
+            txtToUser.Name = "txtToUser";
+            txtToUser.Size = new Size(142, 23);
+            txtToUser.TabIndex = 6;
+            // 
+            // txtTitle
+            // 
+            txtTitle.Location = new Point(36, 30);
+            txtTitle.Name = "txtTitle";
+            txtTitle.Size = new Size(418, 23);
+            txtTitle.TabIndex = 5;
+            // 
+            // datetimeDateSent
+            // 
+            datetimeDateSent.Location = new Point(36, 83);
+            datetimeDateSent.Name = "datetimeDateSent";
+            datetimeDateSent.Size = new Size(200, 23);
+            datetimeDateSent.TabIndex = 13;
+            // 
+            // ReadWriteEmailSystemForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(datetimeDateSent);
+            Controls.Add(btnSend);
+            Controls.Add(richtxtContent);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtToUser);
+            Controls.Add(txtTitle);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "ReadWriteEmailSystemForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Write Email";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSend;
+        private RichTextBox richtxtContent;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox txtToUser;
+        private TextBox txtTitle;
+        private DateTimePicker datetimeDateSent;
+    }
+}
---
+++ PetVaccinationTrackerSystem-Project/Forms/ReadWriteEmailSystemForm.cs
@@ -0,0 +1,74 @@
+﻿using PetVaccinationTrackerSystem_Project.Data;
+using PetVaccinationTrackerSystem_Project.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PetVaccinationTrackerSystem_Project.Forms
+{
+    public partial class ReadWriteEmailSystemForm : Form
+    {
+
+        private User _currentUser;
+
+        public ReadWriteEmailSystemForm(User inUserRef)
+        {
+            InitializeComponent();
+
+            _currentUser = inUserRef;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            using(var context = new ModelContext())
+            {
+                if(int.TryParse(txtToUser.Text.Trim(), out int toSendUserID))
+                {
+                    // Fetch the User Recipient
+                    var userRecipient = context.UserList
+                                    .Where(u => u.UserID == toSendUserID)
+                                    .FirstOrDefault();
+
+                    // Check if the Recipient is Valid
+                    if (userRecipient == null)
+                    {
+                        MessageBox.Show("The User is Invalid!", "Error", MessageBoxButtons.OK,  MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    // Make the email system
+                    var emailToSend = new Email
+                    {
+                        Title = txtTitle.Text.Trim(),
+                        DateAndTimeEmailSent = datetimeDateSent.Value,
+                        Body = richtxtContent.Text.Trim(),
+                        FromUser = _currentUser.UserEmail,
+                        IsRead = false,
+                        IsDeleted = false,
+                        UserID = toSendUserID
+                    };
+
+
+                    context.EmailList.Add(emailToSend);
+                    context.SaveChanges();
+
+                    MessageBox.Show("Email sent successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                } else
+                {
+                    MessageBox.Show("Please enter a valid numeric User ID.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+        }
+
+
+
+    }
+}
