48288cd34c37e6169e5ae5dbecb339f8f6163b12
deanosaurxoxo <odessakernpdecano@iskolarngbayan.pup.edu.ph>
2025-06-26 10:54:03 +0800

    Refined EmailForm, InboxForm, ReadWriteEmailSystemForm UI; Added HomePanel (Pet Owner End)

--- PetVaccinationTrackerSystem-Project/Forms/InboxForm.Designer.cs
+++ PetVaccinationTrackerSystem-Project/Forms/InboxForm.Designer.cs
@@ -28,13 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InboxForm));
             dgvEmails = new DataGridView();
             cmbFilters = new ComboBox();
-            btnSearch = new Button();
             txtSearchBox = new TextBox();
             label1 = new Label();
-            btnWriteEmail = new Button();
+            panel1 = new Panel();
+            mainFormVButtonExit = new Button();
+            btnWriteEmail = new ReaLTaiizor.Controls.HopeRoundButton();
+            btnSearch = new ReaLTaiizor.Controls.HopeRoundButton();
             ((System.ComponentModel.ISupportInitialize)dgvEmails).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // dgvEmails
@@ -42,7 +46,7 @@
             dgvEmails.AllowUserToAddRows = false;
             dgvEmails.BackgroundColor = Color.White;
             dgvEmails.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvEmails.Location = new Point(23, 109);
+            dgvEmails.Location = new Point(25, 110);
             dgvEmails.MultiSelect = false;
             dgvEmails.Name = "dgvEmails";
             dgvEmails.ReadOnly = true;
@@ -52,67 +56,121 @@
             // 
             // cmbFilters
             // 
+            cmbFilters.BackColor = Color.White;
             cmbFilters.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbFilters.Font = new Font("Segoe UI", 10F);
             cmbFilters.FormattingEnabled = true;
-            cmbFilters.Location = new Point(452, 69);
+            cmbFilters.Location = new Point(451, 66);
             cmbFilters.Name = "cmbFilters";
-            cmbFilters.Size = new Size(202, 23);
+            cmbFilters.Size = new Size(225, 25);
             cmbFilters.TabIndex = 7;
-            // 
-            // btnSearch
-            // 
-            btnSearch.Location = new Point(660, 69);
-            btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(117, 23);
-            btnSearch.TabIndex = 8;
-            btnSearch.Text = "Search";
-            btnSearch.UseVisualStyleBackColor = true;
-            btnSearch.Click += btnSearch_Click;
             // 
             // txtSearchBox
             // 
-            txtSearchBox.Location = new Point(149, 69);
+            txtSearchBox.BackColor = Color.White;
+            txtSearchBox.Font = new Font("Segoe UI", 10F);
+            txtSearchBox.Location = new Point(146, 66);
             txtSearchBox.Name = "txtSearchBox";
-            txtSearchBox.Size = new Size(297, 23);
+            txtSearchBox.Size = new Size(295, 25);
             txtSearchBox.TabIndex = 9;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
-            label1.Location = new Point(23, 13);
+            label1.Font = new Font("Segoe UI", 30F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(89, 85, 84);
+            label1.Location = new Point(15, 45);
             label1.Name = "label1";
-            label1.Size = new Size(90, 37);
+            label1.Size = new Size(130, 54);
             label1.TabIndex = 10;
             label1.Text = "Inbox";
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(63, 59, 58);
+            panel1.Controls.Add(mainFormVButtonExit);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(802, 30);
+            panel1.TabIndex = 11;
+            // 
+            // mainFormVButtonExit
+            // 
+            mainFormVButtonExit.BackColor = Color.FromArgb(63, 59, 58);
+            mainFormVButtonExit.BackgroundImage = (Image)resources.GetObject("mainFormVButtonExit.BackgroundImage");
+            mainFormVButtonExit.BackgroundImageLayout = ImageLayout.Zoom;
+            mainFormVButtonExit.FlatAppearance.BorderSize = 0;
+            mainFormVButtonExit.FlatStyle = FlatStyle.Flat;
+            mainFormVButtonExit.Location = new Point(775, 7);
+            mainFormVButtonExit.Name = "mainFormVButtonExit";
+            mainFormVButtonExit.Size = new Size(15, 15);
+            mainFormVButtonExit.TabIndex = 16;
+            mainFormVButtonExit.UseVisualStyleBackColor = false;
+            mainFormVButtonExit.Click += mainFormVButtonExit_Click;
+            // 
             // btnWriteEmail
             // 
-            btnWriteEmail.Location = new Point(660, 27);
+            btnWriteEmail.BackColor = Color.FromArgb(63, 59, 58);
+            btnWriteEmail.BorderColor = Color.FromArgb(220, 223, 230);
+            btnWriteEmail.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnWriteEmail.DangerColor = Color.FromArgb(245, 108, 108);
+            btnWriteEmail.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnWriteEmail.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnWriteEmail.ForeColor = Color.White;
+            btnWriteEmail.HoverTextColor = Color.White;
+            btnWriteEmail.InfoColor = Color.FromArgb(144, 147, 153);
+            btnWriteEmail.Location = new Point(646, 487);
             btnWriteEmail.Name = "btnWriteEmail";
-            btnWriteEmail.Size = new Size(117, 36);
-            btnWriteEmail.TabIndex = 8;
+            btnWriteEmail.PrimaryColor = Color.FromArgb(63, 59, 58);
+            btnWriteEmail.Size = new Size(130, 35);
+            btnWriteEmail.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnWriteEmail.TabIndex = 12;
             btnWriteEmail.Text = "Write Email";
-            btnWriteEmail.UseVisualStyleBackColor = true;
+            btnWriteEmail.TextColor = Color.White;
+            btnWriteEmail.WarningColor = Color.FromArgb(230, 162, 60);
             btnWriteEmail.Click += btnWriteEmail_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BorderColor = Color.FromArgb(220, 223, 230);
+            btnSearch.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnSearch.DangerColor = Color.FromArgb(245, 108, 108);
+            btnSearch.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnSearch.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearch.HoverTextColor = Color.White;
+            btnSearch.InfoColor = Color.FromArgb(144, 147, 153);
+            btnSearch.Location = new Point(689, 61);
+            btnSearch.Name = "btnSearch";
+            btnSearch.PrimaryColor = Color.FromArgb(63, 59, 58);
+            btnSearch.Size = new Size(90, 30);
+            btnSearch.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnSearch.TabIndex = 16;
+            btnSearch.Text = "Search";
+            btnSearch.TextColor = Color.White;
+            btnSearch.WarningColor = Color.FromArgb(230, 162, 60);
+            btnSearch.Click += btnSearch_Click;
             // 
             // InboxForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(802, 497);
+            BackColor = Color.White;
+            ClientSize = new Size(802, 545);
+            Controls.Add(btnSearch);
+            Controls.Add(btnWriteEmail);
+            Controls.Add(panel1);
             Controls.Add(label1);
             Controls.Add(txtSearchBox);
-            Controls.Add(btnWriteEmail);
-            Controls.Add(btnSearch);
             Controls.Add(cmbFilters);
             Controls.Add(dgvEmails);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
+            FormBorderStyle = FormBorderStyle.None;
             Name = "InboxForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Inbox";
             Load += InboxForm_Load;
             ((System.ComponentModel.ISupportInitialize)dgvEmails).EndInit();
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -121,9 +179,11 @@
 
         private DataGridView dgvEmails;
         private ComboBox cmbFilters;
-        private Button btnSearch;
         private TextBox txtSearchBox;
         private Label label1;
-        private Button btnWriteEmail;
+        private Panel panel1;
+        private Button mainFormVButtonExit;
+        private ReaLTaiizor.Controls.HopeRoundButton btnWriteEmail;
+        private ReaLTaiizor.Controls.HopeRoundButton btnSearch;
     }
 }
--- PetVaccinationTrackerSystem-Project/Forms/ReadWriteEmailSystemForm.Designer.cs
+++ PetVaccinationTrackerSystem-Project/Forms/ReadWriteEmailSystemForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnSend = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReadWriteEmailSystemForm));
             richtxtContent = new RichTextBox();
             label3 = new Label();
             label2 = new Label();
@@ -36,98 +36,164 @@
             txtToUser = new TextBox();
             txtTitle = new TextBox();
             datetimeDateSent = new DateTimePicker();
+            panel1 = new Panel();
+            mainFormVButtonExit = new Button();
+            label4 = new Label();
+            btnSend = new ReaLTaiizor.Controls.HopeRoundButton();
+            panel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // btnSend
-            // 
-            btnSend.Location = new Point(604, 397);
-            btnSend.Name = "btnSend";
-            btnSend.Size = new Size(160, 41);
-            btnSend.TabIndex = 12;
-            btnSend.Text = "Send";
-            btnSend.UseVisualStyleBackColor = true;
-            btnSend.Click += btnSend_Click;
             // 
             // richtxtContent
             // 
-            richtxtContent.Location = new Point(36, 122);
+            richtxtContent.BackColor = Color.White;
+            richtxtContent.Location = new Point(34, 183);
             richtxtContent.Name = "richtxtContent";
-            richtxtContent.Size = new Size(728, 257);
+            richtxtContent.Size = new Size(730, 269);
             richtxtContent.TabIndex = 11;
             richtxtContent.Text = "";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(274, 65);
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.ForeColor = Color.FromArgb(89, 85, 84);
+            label3.Location = new Point(622, 125);
             label3.Name = "label3";
-            label3.Size = new Size(142, 15);
+            label3.Size = new Size(134, 19);
             label3.TabIndex = 8;
-            label3.Text = "To ( Provide the User ID ) :";
+            label3.Text = "To (Provide User ID):";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(36, 65);
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.ForeColor = Color.FromArgb(89, 85, 84);
+            label2.Location = new Point(416, 125);
             label2.Name = "label2";
-            label2.Size = new Size(60, 15);
+            label2.Size = new Size(72, 19);
             label2.TabIndex = 9;
             label2.Text = "Date Sent:";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(36, 12);
+            label1.Font = new Font("Segoe UI", 10F);
+            label1.ForeColor = Color.FromArgb(89, 85, 84);
+            label1.Location = new Point(34, 125);
             label1.Name = "label1";
-            label1.Size = new Size(49, 15);
+            label1.Size = new Size(56, 19);
             label1.TabIndex = 10;
             label1.Text = "Subject:";
             // 
             // txtToUser
             // 
-            txtToUser.Location = new Point(274, 83);
+            txtToUser.BackColor = Color.White;
+            txtToUser.Font = new Font("Segoe UI", 10F);
+            txtToUser.Location = new Point(623, 147);
             txtToUser.Name = "txtToUser";
-            txtToUser.Size = new Size(142, 23);
+            txtToUser.Size = new Size(142, 25);
             txtToUser.TabIndex = 6;
             // 
             // txtTitle
             // 
-            txtTitle.Location = new Point(36, 30);
+            txtTitle.BackColor = Color.White;
+            txtTitle.Font = new Font("Segoe UI", 10F);
+            txtTitle.Location = new Point(34, 147);
             txtTitle.Name = "txtTitle";
-            txtTitle.Size = new Size(418, 23);
+            txtTitle.Size = new Size(375, 25);
             txtTitle.TabIndex = 5;
             // 
             // datetimeDateSent
             // 
-            datetimeDateSent.Location = new Point(36, 83);
+            datetimeDateSent.CalendarFont = new Font("Segoe UI", 10F);
+            datetimeDateSent.CalendarMonthBackground = Color.White;
+            datetimeDateSent.Location = new Point(416, 147);
             datetimeDateSent.Name = "datetimeDateSent";
             datetimeDateSent.Size = new Size(200, 23);
             datetimeDateSent.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(63, 59, 58);
+            panel1.Controls.Add(mainFormVButtonExit);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 30);
+            panel1.TabIndex = 14;
+            // 
+            // mainFormVButtonExit
+            // 
+            mainFormVButtonExit.BackColor = Color.FromArgb(63, 59, 58);
+            mainFormVButtonExit.BackgroundImage = (Image)resources.GetObject("mainFormVButtonExit.BackgroundImage");
+            mainFormVButtonExit.BackgroundImageLayout = ImageLayout.Zoom;
+            mainFormVButtonExit.FlatAppearance.BorderSize = 0;
+            mainFormVButtonExit.FlatStyle = FlatStyle.Flat;
+            mainFormVButtonExit.Location = new Point(775, 7);
+            mainFormVButtonExit.Name = "mainFormVButtonExit";
+            mainFormVButtonExit.Size = new Size(15, 15);
+            mainFormVButtonExit.TabIndex = 17;
+            mainFormVButtonExit.UseVisualStyleBackColor = false;
+            mainFormVButtonExit.Click += mainFormVButtonExit_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 30F, FontStyle.Bold);
+            label4.ForeColor = Color.FromArgb(89, 85, 84);
+            label4.Location = new Point(25, 50);
+            label4.Name = "label4";
+            label4.Size = new Size(241, 54);
+            label4.TabIndex = 15;
+            label4.Text = "Write Email";
+            // 
+            // btnSend
+            // 
+            btnSend.BackColor = Color.FromArgb(63, 59, 58);
+            btnSend.BorderColor = Color.FromArgb(220, 223, 230);
+            btnSend.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnSend.DangerColor = Color.FromArgb(245, 108, 108);
+            btnSend.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnSend.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSend.HoverTextColor = Color.White;
+            btnSend.InfoColor = Color.FromArgb(144, 147, 153);
+            btnSend.Location = new Point(655, 468);
+            btnSend.Name = "btnSend";
+            btnSend.PrimaryColor = Color.FromArgb(63, 59, 58);
+            btnSend.Size = new Size(110, 40);
+            btnSend.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnSend.TabIndex = 17;
+            btnSend.Text = "Send";
+            btnSend.TextColor = Color.White;
+            btnSend.WarningColor = Color.FromArgb(230, 162, 60);
+            btnSend.Click += btnSend_Click;
             // 
             // ReadWriteEmailSystemForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(datetimeDateSent);
+            BackColor = Color.White;
+            ClientSize = new Size(800, 535);
             Controls.Add(btnSend);
+            Controls.Add(label4);
+            Controls.Add(panel1);
+            Controls.Add(datetimeDateSent);
             Controls.Add(richtxtContent);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(txtToUser);
             Controls.Add(txtTitle);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
+            FormBorderStyle = FormBorderStyle.None;
             Name = "ReadWriteEmailSystemForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Write Email";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button btnSend;
         private RichTextBox richtxtContent;
         private Label label3;
         private Label label2;
@@ -135,5 +201,9 @@
         private TextBox txtToUser;
         private TextBox txtTitle;
         private DateTimePicker datetimeDateSent;
+        private Panel panel1;
+        private Button mainFormVButtonExit;
+        private Label label4;
+        private ReaLTaiizor.Controls.HopeRoundButton btnSend;
     }
 }
