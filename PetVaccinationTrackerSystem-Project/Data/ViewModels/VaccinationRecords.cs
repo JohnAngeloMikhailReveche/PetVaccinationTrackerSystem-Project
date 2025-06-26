eef3967bf4432a041b5150b90e3db0e8a4c76e18
johnangelomikhailareveche@iskolarngbayan.pup.edu.ph <mikhjnelo@gmail.com>
2025-06-26 21:41:33 +0800

    Fixed Unhandled Exception in Vaccination Instance due to object nullable

---
+++ PetVaccinationTrackerSystem-Project/Data/ViewModels/VaccinationRecords.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetVaccinationTrackerSystem_Project.Data.ViewModels
+{
+    public class VaccinationRecords
+    {
+        public int RecordID { get; set; }
+        public int PetID { get; set; }
+        public string PetName { get; set; }
+        public string Gender { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public string VaccineName { get; set; }
+        public string AdministeredBy { get; set; }
+        public string DateAdministered { get; set; }
+        public string NextDueDate { get; set; }
+        public string BatchNo { get; set; }
+    }
+}
