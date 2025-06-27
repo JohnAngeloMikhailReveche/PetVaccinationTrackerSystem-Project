df058d8b3592897562d10b3686bacdf23f297654
johnangelomikhailareveche@iskolarngbayan.pup.edu.ph <mikhjnelo@gmail.com>
2025-06-27 08:41:53 +0800

    Replaced the structure of using in ImageHelper

--- PetVaccinationTrackerSystem-Project/Classes/ImageHelper.cs
+++ PetVaccinationTrackerSystem-Project/Classes/ImageHelper.cs
@@ -11,8 +11,11 @@ namespace PetVaccinationTrackerSystem_Project.Classes
         public static Image Base64ToImage(string base64String)
         {
             byte[] imageBytes = Convert.FromBase64String(base64String);
-            using var memoryStream = new MemoryStream(imageBytes);
-            return Image.FromStream(memoryStream);
+            using (var memoryStream = new MemoryStream(imageBytes))
+            {
+                return Image.FromStream(memoryStream);
+            }
+            
         }
     }
 }
